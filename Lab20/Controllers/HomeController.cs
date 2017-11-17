a50534aaf8a4ffc8535399230f61c772ba3d82de
David Schwartz <david.schwartzy@gmail.com>
2017-11-17 08:29:48 -0500

    first commit

---
+++ Lab20/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Lab20.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab20.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        public ActionResult ProcessSignup(Register Signup)
+        {
+            ViewBag.FirstName = Signup.FirstName;
+
+            return View();
+        }
+    }
+}
---
+++ Lab20/Models/Register.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Lab20.Models
+{
+    public class Register
+    {
+        private string firstname;
+        private string lastname;
+        private string email;
+        private string phonenumber;
+        private string password;
+
+        public Register() :this("","","","","")
+        {
+           
+        }
+
+        public Register(string firstname, string lastname, string email, string phonenumber, string password)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.email = email;
+            this.phonenumber = phonenumber;
+            this.password = password;
+        }
+
+        public string FirstName
+        {
+            set { firstname = value; }
+            get { return firstname; }
+        }
+
+        public string LastName
+        {
+            set { lastname = value; }
+            get { return lastname; }
+        }
+        public string Email
+        {
+
+            set { email = value; }
+            get { return email; }
+
+        }
+
+        public string PhoneNumber
+        {
+            set { phonenumber = value; }
+            get { return phonenumber; }
+        }
+
+        public string Password
+        {
+            set { password = value; }
+            get { return password; }
+        }
+    }
+}
